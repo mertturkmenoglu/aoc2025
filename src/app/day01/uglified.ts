52c8a2ce24c89869b1b501f4f88e2f4c1896023b
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-12-01 09:06:02 +0300

    update

--- src/app/day01/uglified.ts
+++ src/app/day01/uglified.ts
@@ -1,35 +1,20 @@
-import { defineAocModule, readLines } from "@/lib";
+import { Arr, defineAocModule, type Pos, readLines } from "@/lib";
 
 const lines: string[] = readLines("day01/input.txt");
 
-function sol1(): number {
-	let [dial, counter] = [50, 0];
-
-	for (const ins of lines) {
-		dial = (dial + (ins[0] === "L" ? -+ins.slice(1) : +ins.slice(1))) % 100;
-		if (dial === 0) counter++;
-	}
-
-	return counter;
-}
-
-function sol2(): number {
-	let [dial, counter] = [50, 0];
-
-	for (const ins of lines) {
-		for (let i = 0; i < +ins.slice(1); i++) {
-			dial = (dial + (ins[0] === "L" ? -1 : 1)) % 100;
-			if (dial === 0) counter++;
-		}
-	}
-
-	return counter;
-}
+const A = (n: number, s: string) =>
+	(n + (s[0] === "L" ? -+s.slice(1) : +s.slice(1))) % 100;
+const B = (n: number, c: number) =>
+	n === 0 ? ([n, c + 1] as Pos) : ([n, c] as Pos);
+const C = (s: string) => Arr.indices(+s.slice(1));
+const E = (n: number, s: string) => (n + (s[0] === "L" ? -1 : 1)) % 100;
+const F = ([D, c]: Pos, I: string) =>
+	C(I).reduce(([D, c]) => B(E(D, I), c), [D, c] as Pos);
 
 export default defineAocModule({
 	day: 1,
 	exp1: 1026,
 	exp2: 5923,
-	sol1,
-	sol2,
+	sol1: () => lines.reduce(([D, c], I) => B(A(D, I), c), [50, 0] as Pos)[1],
+	sol2: () => lines.reduce(F, [50, 0] as Pos)[1],
 });
