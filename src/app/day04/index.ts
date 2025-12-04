b840d6ff5ed43b1e8cb9f274075361845b42d7d1
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-12-04 08:20:45 +0300

    update

--- src/app/day04/index.ts
+++ src/app/day04/index.ts
@@ -1,3 +1,4 @@
+/** biome-ignore-all lint/style/noNonNullAssertion: No need */
 import {
 	cardinalCoefs,
 	defineAocModule,
@@ -9,12 +10,40 @@ import {
 } from "@/lib";
 
 const lines: string[] = readLines("day04/input.txt");
+const coefs = [...cardinalCoefs, ...diagonalCoefs];
+const M: Matrix<string> = lines.map((l) => l.split(""));
 
 function sol1(): number {
-	const m: Matrix<string> = lines.map((l) => l.split(""));
 	let counter = 0;
-	const coefs = [...cardinalCoefs, ...diagonalCoefs];
 
+	traverse(M, () => counter++);
+
+	return counter;
+}
+
+function sol2(): number {
+	let totalCounter = 0;
+
+	while (true) {
+		const toBeRemoved: Pos[] = [];
+
+		traverse(M, (pos) => toBeRemoved.push(pos));
+
+		if (toBeRemoved.length === 0) {
+			break;
+		}
+
+		for (const p of toBeRemoved) {
+			Mtr.set(M, p, ".");
+		}
+
+		totalCounter += toBeRemoved.length;
+	}
+
+	return totalCounter;
+}
+
+function traverse(m: Matrix<string>, fn: (pos: Pos) => void): void {
 	for (let i = 0; i < m.length; i++) {
 		for (let j = 0; j < m[0]!.length; j++) {
 			const el = Mtr.at(m, [i, j]);
@@ -34,65 +63,16 @@ function sol1(): number {
 			}
 
 			if (nc < 4) {
-				counter++;
+				fn([i, j]);
 			}
 		}
 	}
-
-	return counter;
-}
-
-function sol2(): number {
-	const m: Matrix<string> = lines.map((l) => l.split(""));
-	let flag = true;
-	const coefs = [...cardinalCoefs, ...diagonalCoefs];
-	let totalCounter = 0;
-
-	while (flag) {
-		const toBeRemoved: Pos[] = [];
-
-		for (let i = 0; i < m.length; i++) {
-			for (let j = 0; j < m[0]!.length; j++) {
-				const el = Mtr.at(m, [i, j]);
-
-				if (el !== "@") {
-					continue;
-				}
-
-				let nc = 0;
-
-				for (const [dr, dc] of coefs) {
-					const neighbour = Mtr.$at(m, [i + dr, j + dc]);
-
-					if (neighbour === "@") {
-						nc++;
-					}
-				}
-
-				if (nc < 4) {
-					toBeRemoved.push([i, j]);
-				}
-			}
-		}
-
-		if (toBeRemoved.length === 0) {
-			flag = false;
-		} else {
-			for (const p of toBeRemoved) {
-				Mtr.set(m, p, ".");
-			}
-		}
-
-		totalCounter += toBeRemoved.length;
-	}
-
-	return totalCounter;
 }
 
 export default defineAocModule({
 	day: 4,
 	exp1: 1464,
-	exp2: 0,
+	exp2: 8409,
 	sol1,
 	sol2,
 });
