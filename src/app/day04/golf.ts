73a392efe1d7bac71f7007e7e70131bc173887a1
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-12-04 09:04:14 +0300

    add code golf version

---
+++ src/app/day04/golf.ts
@@ -0,0 +1,38 @@
+/** biome-ignore-all lint/style/noNonNullAssertion: No need */
+import {
+	Arr,
+	cardinalCoefs,
+	defineAocModule,
+	diagonalCoefs,
+	type Matrix,
+	Mtr,
+	type Pos,
+	readLines,
+} from "@/lib";
+
+type Fn = (pos: Pos[]) => void;
+type Z = number | Promise<number>;
+
+const lines: string[] = readLines("day04/input.txt");
+const coefs = [...cardinalCoefs, ...diagonalCoefs];
+const A: Pos[] = [];
+const M: Matrix<string> = lines.map((l) => l.split(""));
+const N = ([i, j]: Pos) =>
+	coefs.map(([dr, dc]) => Mtr.$at(M, [i + dr, j + dc]));
+const C = ([i, j]: Pos) => N([i, j]).filter((x) => x === "@").length < 4;
+const T = async (F: Fn) =>
+	Mtr.forEachCell(M, (el, i, j) => F(el === "@" && C([i, j]) ? [[i, j]] : []));
+const B = () => new Set(A.map((x) => x.join(","))).size;
+const D = () => Promise.resolve(Arr.rep(A, (p) => Mtr.set(M, p, ".")));
+const E = () => T((pos) => A.push(...pos));
+const F = () => A.length;
+const G = (P = F()) =>
+	E().then((): Z => (P === F() ? B() : D().then(() => G(F()))));
+
+export default defineAocModule({
+	day: 4,
+	exp1: 1464,
+	exp2: 8409,
+	sol1: () => T((pos) => A.push(...pos)).then(() => A.length),
+	sol2: () => G(),
+});
