ed35c55fc5e42047b038098b5e9793e8d2b5abe7
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-12-06 08:42:18 +0300

    add solution

--- src/app/day06/index.ts
+++ src/app/day06/index.ts
@@ -1,19 +1,78 @@
-import { defineAocModule, readLines } from "@/lib";
+import { Arr, defineAocModule, Mtr, readLines } from "@/lib";
 
-const _lines: string[] = readLines("day06/input.txt");
+const lines: string[][] = readLines("day06/input.txt").map((l) =>
+	l
+		.trim()
+		.split(" ")
+		.map((x) => x.trim())
+		.filter((x) => x !== ""),
+);
 
 function sol1(): number {
-	return 0;
+	const cols = lines[0]!.length;
+	let grand = 0;
+
+	for (let i = 0; i < cols; i++) {
+		const col = Mtr.col(lines, i);
+		const numbers = col.slice(0, col.length - 1);
+		const operation = col.at(-1)!;
+
+		let acc = operation === "+" ? 0 : 1;
+
+		for (const num of numbers) {
+			acc = operation === "+" ? acc + +num : acc * +num;
+		}
+
+		grand += acc;
+	}
+
+	return grand;
 }
 
 function sol2(): number {
-	return 0;
+	let original = readLines("day06/input.txt");
+	const colsCount = lines[0]!.length;
+
+	let grand = 0;
+
+	for (let i = 0; i < colsCount; i++) {
+		const col = Mtr.col(lines, i);
+		const maxLength = Arr.max(col.map((x) => x.length));
+		const values = original.map((line) =>
+			line.slice(0, maxLength).replaceAll(" ", "0"),
+		);
+
+		const numbers = values.slice(0, values.length - 1);
+		const operation = values.at(-1)!.replaceAll("0", "");
+
+		let acc = operation === "+" ? 0 : 1;
+
+		const nums: string[] = [];
+
+		for (let j = 0; j < maxLength; j++) {
+			const a = numbers
+				.map((x) => x[j]!)
+				.join("")
+				.replaceAll("0", "");
+			nums.push(a);
+		}
+
+		for (const num of nums) {
+			acc = operation === "+" ? acc + +num : acc * +num;
+		}
+
+		grand += acc;
+
+		original = original.map((x) => x.slice(maxLength + 1));
+	}
+
+	return grand;
 }
 
 export default defineAocModule({
 	day: 6,
-	exp1: 0,
-	exp2: 0,
+	exp1: 6_957_525_317_641,
+	exp2: 13_215_665_360_076,
 	sol1,
 	sol2,
 });
