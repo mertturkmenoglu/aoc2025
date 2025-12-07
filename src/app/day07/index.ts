6d0575b4b60db87d57f2db7ab0949bdfcb1b81b8
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-12-07 08:13:43 +0300

    add day 7 solution 1

--- src/app/day07/index.ts
+++ src/app/day07/index.ts
@@ -1,9 +1,42 @@
-import { defineAocModule, readLines } from "@/lib";
+import { defineAocModule, type Matrix, Mtr, readLines } from "@/lib";
 
-const _lines: string[] = readLines("day07/input.txt");
+const mtr = readLines("day07/input.txt").map((l) =>
+	l.split(""),
+) as Matrix<string>;
 
 function sol1(): number {
-	return 0;
+	let splits = 0;
+
+	// Add first bean
+	Mtr.set(mtr, [1, mtr[0]!.indexOf("S")], "|");
+
+	for (let r = 0; r < mtr.length; r++) {
+		for (let c = 0; c < mtr[r]!.length; c++) {
+			const cell = Mtr.at(mtr, [r, c]);
+
+			if (cell === "|") {
+				// Get the bottom cell
+				const bottom = Mtr.$at(mtr, [r + 1, c]);
+				// If the bottom is a splitter, split
+				if (bottom === "^") {
+					// Create two beans
+					// Check boundaries
+					if (Mtr.isOnGrid(mtr, [r + 1, c - 1])) {
+						Mtr.set(mtr, [r + 1, c - 1], "|");
+					}
+					if (Mtr.isOnGrid(mtr, [r + 1, c + 1])) {
+						Mtr.set(mtr, [r + 1, c + 1], "|");
+					}
+					splits += 1;
+				} else if (bottom === ".") {
+					// else if the bottom is empty '.' char, replace with bean
+					Mtr.set(mtr, [r + 1, c], "|");
+				}
+			}
+		}
+	}
+
+	return splits;
 }
 
 function sol2(): number {
