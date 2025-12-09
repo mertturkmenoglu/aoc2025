34ee194e56a3d5dcc5abbce471595a75c6da021f
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-12-09 09:05:56 +0300

    add part 1 golf

--- src/app/day09/golf.ts
+++ src/app/day09/golf.ts
@@ -1,10 +1,12 @@
-import { defineAocModule, readLines } from "@/lib";
+/** biome-ignore-all lint/style/useConst: <explanation> */
+import { Arr, defineAocModule, type Pos, readLines } from "@/lib";
 
-const _lines: string[] = readLines("day09/input.txt");
+let L = readLines("day09/input.txt").map(
+	(l) => l.split(",").map(Number) as Pos,
+);
 
-function sol1(): number {
-	return 0;
-}
+let D = ([x1, y1]: Pos, [x2, y2]: Pos) =>
+	(Math.abs(x2 - x1) + 1) * (Math.abs(y2 - y1) + 1);
 
 function sol2(): number {
 	return 0;
@@ -14,6 +16,6 @@ export default defineAocModule({
 	day: 9,
 	exp1: 0,
 	exp2: 0,
-	sol1,
+	sol1: () => Arr.max(L.flatMap((p1) => L.map((p2) => D(p1, p2)))),
 	sol2,
 });
