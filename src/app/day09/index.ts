deee20b3a6a6d50c8cfe6a48ef71d5a539e39f8c
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-12-09 08:12:43 +0300

    add part 1

--- src/app/day09/index.ts
+++ src/app/day09/index.ts
@@ -1,9 +1,26 @@
-import { defineAocModule, readLines } from "@/lib";
+import { defineAocModule, type Pos, readLines } from "@/lib";
 
-const _lines: string[] = readLines("day09/input.txt");
+const lines = readLines("day09/input.txt").map(
+	(l) => l.split(",").map(Number) as Pos,
+);
 
 function sol1(): number {
-	return 0;
+	let area = -1;
+
+	for (let i = 0; i < lines.length; i++) {
+		for (let j = 0; j < lines.length; j++) {
+			const [x1, y1] = lines[i]!;
+			const [x2, y2] = lines[j]!;
+
+			const newArea = (Math.abs(x2 - x1) + 1) * (Math.abs(y2 - y1) + 1);
+
+			if (newArea > area) {
+				area = newArea;
+			}
+		}
+	}
+
+	return area;
 }
 
 function sol2(): number {
